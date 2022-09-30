5c5480fde5ba09d27efe0a654e7b3e208766c636
Minseok Choi <alstjrchl97@likelion.org>
2022-09-30 11:46:19 +0900

    ✨feat: posting 2022-09-30
    Affected files:
    package.json
    src/components/ProfileCard/index.tsx
    src/layouts/CustomLayout.tsx
    src/pages/about.tsx
    yarn.lock

---
+++ src/components/ProfileCard/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ProfileCardProps {
+  name: string;
+  description: string;
+}
+
+export const ProfileCard = ({ name, description }: ProfileCardProps) => {
+  return <div>index</div>;
+};
+
+const ImageBox = styled.div`
+  width: 100%;
+`;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import { CustomLayout } from "../layouts/CustomLayout";
 const About = () => {
-  return <Layout>안녕하세요 프론트엔드 개발자 최민석입니다.</Layout>;
+  return (
+    <Layout>
+      <CustomLayout maxWidth={660}>
+        안녕하세요 프론트엔드 개발자 최민석입니다.
+      </CustomLayout>
+    </Layout>
+  );
 };
 
 export default About;
